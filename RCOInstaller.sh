2f1dd2d835bcb2a0290e6efd02f9faeaac8f1bc2
Benedict Stanley <56202480+benedictstanley@users.noreply.github.com>
2023-06-30 10:12:59 -0500

    Update RCOInstaller.sh
    Signed-off-by: Benedict Stanley <56202480+benedictstanley@users.noreply.github.com>

--- RCOInstaller.sh
+++ RCOInstaller.sh
@@ -1,8 +1,8 @@
-cd /Applications/Roblox.app/Contents/MacOS
+cd /Users/benstanley/Applications/Roblox.app/Contents/MacOS
 if [ ! -f RCOPATCHED ]; then
 mv RobloxPlayer ActualRobloxPlayer
 echo "#!/bin/sh" >> RobloxPlayer
-echo "rm -rf /Applications/Roblox.app/Contents/MacOS/ClientSettings && mkdir /Applications/Roblox.app/Contents/MacOS/ClientSettings && curl https://roblox-client-optimizer.simulhost.com/ClientAppSettings.json -o /Applications/Roblox.app/Contents/MacOS/ClientSettings/ClientAppSettings.json && exec /Applications/Roblox.app/Contents/MacOS/ActualRobloxPlayer" >> RobloxPlayer
+echo "rm -rf /Users/benstanley/Applications/Roblox.app/Contents/MacOS/ClientSettings && mkdir /Users/benstanley/Applications/Roblox.app/Contents/MacOS/ClientSettings && curl https://roblox-client-optimizer.simulhost.com/ClientAppSettings.json -o /Users/benstanley/Applications/Roblox.app/Contents/MacOS/ClientSettings/ClientAppSettings.json && exec /Users/benstanley/Applications/Roblox.app/Contents/MacOS/ActualRobloxPlayer" >> RobloxPlayer
 chmod +x RobloxPlayer
 touch RCOPATCHED
 echo "Finished installing RCO"
